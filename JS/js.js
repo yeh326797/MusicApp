e9cbe6477b29a7fe4eb133b0f62f24e7cb4b1a5c
Pearl Yeh <yeh326797@gmail.com>
2021-05-19 01:50:04 +0800

    favorite function

--- JS/js.js
+++ JS/js.js
@@ -1,21 +1,20 @@
 var button = $(".play-btn");
 
+//播放鍵切換
 button.on("click", function () {
   button.toggleClass("active");
   $("#cover").find("img").toggleClass("active");
 });
 
-
-
-
+//音軌
 var allTruck = document.querySelectorAll(".track");
-let index = 0;
-let animation;
+var index = 0;
+var animation;
 
 button.on("click", () => {
   if (button.hasClass("active")) {
     animation = setInterval(() => {
-      allTruck[index].classList.add('black');
+      allTruck[index].classList.add("black");
       index = index + 1;
     }, 1000);
   } else {
@@ -23,5 +22,15 @@ button.on("click", () => {
   }
 });
 
+//我的最愛
 
-  
+var blueHeart = $(".blue-heart");
+var redHeart = $(".heart");
+
+blueHeart.on("click", function () {
+  blueHeart.toggleClass("check");
+});
+
+redHeart.on("click", function () {
+  redHeart.toggleClass("check");
+});
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # MusicApp
+  
+  參考網站：https://codepen.io/TurkAysenur/pen/povVmwO
+  
+  + 音樂播放介面切版練習
+  + 音樂播放動畫(CD旋轉、音軌動畫)
+
+
+### Link
+https://yeh326797.github.io/MusicApp/
